dee0111f1bd6316582c9da36a22113eafdae67ac
Chust3r <kriptoortega@gmail.com>
2024-09-08 16:46:06 -0600

    Reaname API endpoint

--- app/api/index.ts
+++ app/api/index.ts
@@ -1,6 +1,6 @@
 import { Hono } from 'hono'
 import { api_auth } from './auth/index'
 
-export const api = new Hono().basePath('/api/v1')
+export const api = new Hono().basePath('/api')
 
 api.route('/', api_auth)
