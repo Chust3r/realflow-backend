903c226786fc673ede8931a3ba34dd6845b0871b
Chust3r <kriptoortega@gmail.com>
2024-09-08 18:55:56 -0600

    Modularize WS HANDLERS

---
+++ app/ws/handlers.ts
@@ -0,0 +1,37 @@
+import type { Peer } from 'crossws'
+import { getQueryParam } from 'hono/utils/url'
+import { verify } from '~lib/jwt'
+
+interface IMessage {
+	timestamp: string
+	event: string
+	payload?: Record<string, any>
+}
+
+const createMessage = (event: string, payload?: Record<string, any>) => {
+	const m: IMessage = {
+		timestamp: new Date().toISOString(),
+		event,
+		payload,
+	}
+
+	return JSON.stringify(m)
+}
+
+export const open = async (peer: Peer) => {
+	const t = getQueryParam(peer.request?.url!, 't') as string
+
+	const token = await verify(t)
+
+	//→ IF TOKEN IS INVALID CLOSE
+
+	if (!token) peer.close()
+
+	const roomId = token?.roomId
+
+	
+
+	const msg = createMessage('open')
+
+	peer.send(msg)
+}
--- app/ws/index.ts
+++ app/ws/index.ts
@@ -1,12 +1,10 @@
 import crossws from 'crossws/adapters/node'
-import { getQueryParam } from 'hono/utils/url'
+import { open } from './handlers'
 
 export const roomHandler = crossws({
 	hooks: {
 		upgrade: () => {},
-		open: async (peer) => {
-			const t = getQueryParam(peer.request?.url!, 't') as string
-		},
+		open,
 		close: () => {},
 		error: () => {},
 		message: () => {},
