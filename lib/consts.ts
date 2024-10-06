2fba7b1384547da52de28276e62c2fd2da5feafb
Chust3r <kriptoortega@gmail.com>
2024-10-06 15:54:57 -0600

    Add CONSTS

--- lib/consts.ts
+++ lib/consts.ts
@@ -3,3 +3,9 @@ import 'dotenv/config'
 export const PORT = process.env.PORT || 7000
 
 export const REDIS_URL = process.env.REDIS_URL || 'redis://localhost:6379'
+
+export const AUTH_SECRET = process.env.AUTH_SECRET || ''
+
+export const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID || ''
+
+export const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET || ''
