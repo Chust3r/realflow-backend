3d7dfeedb4083cd2fd317b3c609daaad932c23dc
Chust3r <kriptoortega@gmail.com>
2024-10-06 17:59:05 -0600

    Add logger

--- index.ts
+++ index.ts
@@ -1,13 +1,18 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
 import { cors } from 'hono/cors'
+import { logger } from 'hono/logger'
 import { websocket, api } from '~routes'
 
 const app = new Hono()
 
 app.use(cors())
 
+app.use(logger())
+
 app.route('/', api)
+
+console.log('Listening on http://localhost:4000')
 
 const server = serve({
 	fetch: app.fetch,
